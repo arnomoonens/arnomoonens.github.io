70a3bcb147677ec8d7765dd89a26f48dee42c1df
Arno Moonens <arno.moonens@gmail.com>
2022-12-29 20:33:38 +0100

    Changed work and about text

--- src/About.svelte
+++ src/About.svelte
@@ -6,5 +6,9 @@
 <Page on:close>
     <h2 class="major">About</h2>
     <span class="image main"><img src={image} alt="Electric longboard" /></span>
-    <p>Under construction.</p>
+    <p>Welcome to my About page! My name is Arno and I am a data scientist/machine learning engineer by day and a fitness enthusiast, language learner, and FIFA champion by night. </p>
+    <p>When I'm not busy crunching numbers and creating algorithms, you can find me hitting the gym or trying to become fluent in Italian (although I'll admit, my "Ciao" still sounds more like a botched attempt at a reverse sneeze). </p>
+    <p>But don't let my love for fitness and linguistics fool you, I am also a nerd at heart and can often be found buried in a non-fiction book or tinkering with the latest machine learning techniques (because let's be real, learning about machine learning is just as fun as actually doing it).</p>
+    <p>In my spare time, I like to take long walks (because who doesn't love some good cardio?), and challenge my friends to a game of FIFA (where I will inevitably emerge victorious). </p>
+    <p>Thanks for stopping by and getting to know me a little better. I hope you have a great day!</p>
 </Page>
--- src/Work.svelte
+++ src/Work.svelte
@@ -6,5 +6,37 @@
 <Page on:close>
     <h2 class="major">Work</h2>
     <span class="image main"><img src={image} alt="Charging electric vehicle" /></span>
-    <p>Under construction.</p>
+    <p>I work at <a href="https://unipartners.be">UniPartners</a>, where I am a data scientist and machine learning engineer.</p>
+    <p>As of programming languages, I use Python most of the time, but I also know my way around SQL, C, C++, Bash, HTML, TypeScript and JavaScript.</p>
+    <p>When doing data science or building machine learning applications with Python, I use the following packages or tools:</p>
+    <ul>
+        <li>PyTorch</li>
+        <li>TensorFlow</li>
+        <li>NumPy</li>
+        <li>Scikit-Learn</li>
+        <li>OpenAI Gym</li>
+        <li>Numba</li>
+        <li>Matplotlib</li>
+        <li>Plotly</li>
+        <li>Jupyter Notebook</li>
+        <li>Streamlit</li>
+        <li>Flask</li>
+        <li>cvxopt</li>
+        <li>Optuna</li>
+        <li>requests</li>
+        <li>Sphinx</li>
+        <li>Beautifulsoup</li>
+    </ul>
+
+    <p>You can see me using some of these packages in my public repositories on <a href="https://github.com/arnomoonens">GitHub</a>.</p>
+    <p>In terms of databases, I have experience with Microsoft SQL Server, MySQL and PostgreSQL.</p>
+    <p>Last, I also have experience with the following tools and services:</p>
+    <ul>
+        <li>Git</li>
+        <li>Amazon Web Services</li>
+        <li>Microsoft Office</li>
+        <li>Visual Studio Code</li>
+        <li>Vim</li>
+        <li>Slurm Workload Manager</li>
+    </ul>
 </Page>
